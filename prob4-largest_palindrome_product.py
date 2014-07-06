1c56fabede90186a55a930a0042219fc0cedd12a
ctlewitt <ctlewitt@users.noreply.github.com>
2014-07-06 19:10:52 -0400

    Create prob4-largest_palindrome_product.py
    Initial draft and thoughts on algorithm.  Very very rough.

---
+++ prob4-largest_palindrome_product.py
@@ -0,0 +1,16 @@
+#testing work with 2 digit numbers and 4 digit palindrome products
+#later, the project will be 3 digit numbers and 9 digit palindrome products
+
+num1 = 99
+num2 = 99
+
+first_half_num = 99
+#find reverse_num
+#put first_half_num and reverse_num together for full_num
+
+
+check divisible #but only check while first num > second num
+if not divisible, increment first_half_num down and redo
+
+#99, 98, 97, 96, etc will find the greatest palindromes: 9999, 9889, 9669
+#since we are looking for the largest product, we should begin by dividing by the largest (2 digit) integers
