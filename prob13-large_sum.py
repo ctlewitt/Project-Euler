001b0b3d40bbba9f9f94f136f62befefd18308a4
ctlewitt <jasoncyip@jasons-macbook.home>
2014-07-13 11:45:55 -0400

    created files for prob 13, and started thinking about the problem….

---
+++ prob13-large_sum.py
@@ -0,0 +1,13 @@
+#read in numbers
+#might need to make them be "long"s
+#add them together
+#turn into string to find the length
+#slice the first 10 digits
+#print out (or turn into number, whatever you like)
+
+#Something tells me it won't be this simple....based on number size....
+#If it's not....
+#Maybe I only need to add up the beginnings of the numbers
+#How many extra digits would I need?
+#How long does it take for "carrying" to propagate?
+#We are adding numbers 100 times, after all....
