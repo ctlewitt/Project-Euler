eee9b0728a6a2d692ad05449f3fdf1e31d8eb498
ctlewitt <ctlewitt@gmail.com>
2014-07-16 17:04:07 -0400

    faster

--- prob14-longest_collatz_sequence.py
+++ prob14-longest_collatz_sequence.py
@@ -5,6 +5,8 @@
 #e.g., 13 produces a subsequence of 40 (13*3+1=40), which has 1 fewer steps than the 13 sequence
 #so, when starting an even number, always check if it's in the keys before calculating it
 
+import time
+start = time.time()
 MAX = 1000000
 
 def is_divisible(num, divisor):
@@ -14,56 +16,60 @@ def is_divisible(num, divisor):
     else:
         return False
 
-def collatz_sequence(n_temp, collatz):
+def collatz_sequence(n_temp):
     #check if repeat number
-    if n_temp in collatz:
 #        if is_even == False:
 #            if n_temp*2 not in collatz.keys(): ADD BACK IN TO SEE IF FASTER
 #            collatz[n_temp*2] = collatz[n_temp] + 1
-        return [n_temp], collatz[n_temp]
-    else:
-        mini_collatz = [n_temp]
-        seq_len = 1
-        while n_temp != 1:
-            #find next number in sequence
-            if is_divisible(n_temp, 2):
-                n_temp /= 2
-            else:
-                n_temp = 3 * n_temp + 1
-            #decide if next number is new. if new, add it and proceed.  if not new, use old data and stop
-            if n_temp in collatz.keys():
-                seq_len += collatz[n_temp]
-                break
-            else:
-                mini_collatz.append(n_temp)
-                seq_len += 1
-#            print "n_temp: ",
-#            print n_temp
-#    print "mini_collatz:",
-#    print mini_collatz
+    mini_collatz = [n_temp]
+    seq_len = 1
+    while n_temp > 1:
+        #find next number in sequence
+        if is_divisible(n_temp, 2):
+            n_temp /= 2
+        else:
+            n_temp = 3 * n_temp + 1
+        #decide if next number is new. if new, add it and proceed.  if not new, use old data and stop
+#        if n_temp in collatz.keys():
+#            seq_len += collatz[n_temp]
+#            break
+#        else:
+#            mini_collatz.append(n_temp)
+        seq_len += 1
     return mini_collatz, seq_len
 
 MAX = 1000000
 HALF_MAX = MAX/2
 is_even = False
 collatz = {}
-for n in range(333333, MAX):
-    mini_collatz, seq_len = collatz_sequence(n, collatz)
-    for i, num in enumerate(mini_collatz):
-        collatz[num] = seq_len - i
-    #if the starting number is odd, the sequence starting with double that number has one more number
+max = [1,1] #[starting_number, sequence_length]
+#skip_list = range(6)
 
-    if n <= HALF_MAX:
-        collatz[n*2] = seq_len + 1
+skipper = 1
 
-max = [1,1]
-for n in collatz.keys():
-    if collatz[n]>max[1]:
-        max = [n, collatz[n]]
+#for n in range(HALF_MAX, MAX):
+for n in range(HALF_MAX, MAX):
+    #cycle i
+    if skipper < 5:
+        skipper += 1
+    else:
+        skipper = 0
 
-print collatz
-print max
-print collatz[910107]
+    if skipper not in [2, 4, 5]: #takes care of 4(mod6) and 2(mod3) cases.
+        #see this page for explanation http://math.stackexchange.com/questions/60573/reducing-the-time-to-calculate-collatz-sequences
+        #if already done, skip
+#        if n not in collatz.keys():
+        mini_collatz, seq_len = collatz_sequence(n)
+        #enter in steps for each number in sequence to save time later
+        collatz[n] = seq_len
 
-#for each number in the list
-#for each odd number, double the starter and add 1 number to the steps
+
+
+        #for i, num in enumerate(mini_collatz):
+        #    collatz[num] = seq_len - i
+        #check for new max sequence length
+        if seq_len > max[1]:
+            max = [mini_collatz[0], seq_len]
+
+elapsed = (time.time() - start)
+print "The longest sequence starts with %s and is %s digits long. \nIt took %s second to find the solution" % (max[0], max[1], elapsed)
