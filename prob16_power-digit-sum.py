523c069efe85bf84cb78f5c6b704696b4087b80f
ctlewitt <jasoncyip@jasons-macbook.home>
2014-07-17 23:51:15 -0400

    finished problem 16

--- prob16_power-digit-sum.py
+++ prob16_power-digit-sum.py
@@ -1,3 +1,7 @@
+import time
+
+start = time.time()
+
 num = 2**1000
 num_str = str(num)
 sum = 0
@@ -5,3 +9,5 @@ for char in num_str:
     sum += int(char)
 
 print sum
+time_elapsed = time.time() - start
+print "it took " + str(time_elapsed) + " seconds."
