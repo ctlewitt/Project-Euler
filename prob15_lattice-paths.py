3a8e5e8e7c5afa096dda7fcb69f67afd8d018c9f
ctlewitt <ctlewitt@gmail.com>
2014-07-18 15:35:37 -0400

    removed test printouts, works for 6x6

--- prob15_lattice-paths.py
+++ prob15_lattice-paths.py
@@ -1,7 +1,7 @@
 from time import time
 
 start = time()
-DIM = 6
+DIM = 7
 
 
 def add_col(size):
@@ -44,55 +44,41 @@ step = 1
 #step 1) 1
 if DIM+1 >= step:
     paths += 1
-    print "step:" + str(step)
-    print "paths:" + str(paths)
     step += 1
 
 #step 2) dim
 if DIM+1 >= step:
     paths += DIM
-    print "step:" + str(step)
-    print "paths:" + str(paths)
     step += 1
 
 
 #step 3) 1 to dim
 if DIM+1 >= step:
     paths += add_col(DIM)
-    print "step:" + str(step)
-    print "paths:" + str(paths)
     step += 1
 
 #step 4) dim-triangle
 if DIM+1 >= step:
     paths += add_triangle(DIM)
-    print "step:" + str(step)
-    print "paths:" + str(paths)
     step += 1
 
 #step 5) 1-triangle to dim-triangle
 if DIM+1 >= step:
     paths += add_triangle_progression(DIM)
-    print "step:" + str(step)
-    print "paths:" + str(paths)
     step += 1
 
 #steps 6 to DIM+1) {1-triangle to dim-triangle}, {1-triangle to (dim-1)-triangle}, ..., {1-triangle}
 multiplier = 1
-while DIM+2 >= step:
+while DIM+1 >= step:
     paths += add_reducing_triangle_progressions(DIM)
-    print "step:" + str(step)
-    print "paths:" + str(paths)
     step += 1
+
+if DIM >=6:
+    paths += add_reducing_triangle_progressions(DIM)
+    paths -= 2*(add_col(DIM))
 
 print "There are %s possible paths you can take to traverse a %s by %s lattice." %(paths, DIM, DIM)
 
 end = time()
 print "it took %s seconds" %(end-start)
 
-#quick test
-test = 0
-for num in range(1, 22):
-    test += num * (DIM +2 - num)
-    print str(test) + "=" + str(num) + "*" + str(DIM+2-num)
-print "test" + str(test)
