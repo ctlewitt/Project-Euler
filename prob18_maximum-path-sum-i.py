28a9de6f618df4e94bb58ab3cd59d8c1ddd79260
ctlewitt <ctlewitt@gmail.com>
2014-07-21 12:03:01 -0400

    completed problem 18--my first recursion program!!!

--- prob18_maximum-path-sum-i.py
+++ prob18_maximum-path-sum-i.py
@@ -1,12 +1,19 @@
+from time import time
+
+#I know this isn't the most elegant solution, but it is my first program that uses recursion, and I'm really proud!!!  :)
+
+start = time()
+
+ROW = 0
+COL = 1
+#last_position = [row, col]
+
 def read_in_triangle(file_name):
     triangle = []
-    mini_list = []
     with open(file_name) as f:
         for line in f:
             mini_list = line.split()
-            print mini_list
             triangle.append(mini_list)
-    print triangle
     triangle = turn_into_numbers(triangle)
     return triangle
 
@@ -19,14 +26,51 @@ def turn_into_numbers(str_list):
         num_list.append(mini_num_list)
     return num_list
 
-triangle = read_in_triangle("prob18_triangle.txt")
-num_count = 0
-sum = 0
-for row in triangle:
-    for col in row:
-        num_count += 1
-        sum += col
+def find_all_paths(current_path, last_position, triangle, all_paths):
+    if last_position[ROW] == len(triangle) - 1:
+        all_paths.append(current_path)
+        return current_path
+    else:
+        current_path0 = list(current_path)
+        current_path1 = list(current_path)
 
-print sum
-print num_count
-print "average number: " + str(sum/num_count)
+        next_position0 = [last_position[ROW] + 1, last_position[COL]]
+        next_position1 = [last_position[ROW] + 1, last_position[COL] + 1]
+
+        current_path0.append(triangle[next_position0[ROW]][next_position0[COL]])
+        current_path1.append(triangle[next_position1[ROW]][next_position1[COL]])
+
+        find_all_paths(current_path0, next_position0, triangle, all_paths)
+        find_all_paths(current_path1, next_position1, triangle, all_paths)
+
+#get triangle
+triangle = read_in_triangle("prob18_triangle.txt")
+
+#initialize variables
+all_paths = []
+current_path = []
+#starting position and first partial current_path (made of starting position in triangle)
+position = [0, 0]
+current_path.append(triangle[0][0])
+
+#find all paths in triangle (recursive function)
+find_all_paths(current_path, position, triangle, all_paths)
+
+#find path whose members have the highest sum
+sum = 0
+max = 0
+winner = []
+for path in all_paths:
+    sum = 0
+    for step in path:
+        sum += step
+    if sum > max:
+        max = sum
+        winner = path
+
+end = time()
+
+#report back
+print "winning path: " + str(winner)
+print "maximum sum of path steps: " + str(max)
+print "This program took %s seconds to run" %(end-start)
