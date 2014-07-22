4ecc5ecf765f58a6109a1e3aee99317d76bb1e24
ctlewitt <ctlewitt@gmail.com>
2014-07-22 16:15:21 -0400

    outlined ideas for problem 23

---
+++ prob23_non-abundant-sums.py
@@ -0,0 +1,9 @@
+#find abundant numbers from 12 to 28123
+
+#make list of numbers that are sums of 2 abundant numbers
+
+#sort list of abundant number sums
+
+#find sum of all numbers from 1 to 28123 that are not in the list of abundant number sums
+    #this is slightly faster because I don't have to check repeatedly if the numbers are in the abundant number sums list
+#print results
